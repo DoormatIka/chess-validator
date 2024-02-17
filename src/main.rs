98acebad8fd0ea23b875c91af5d5d53c55b7bc2c
DoormatIka <trashcanartistlily@gmail.com>
2024-02-17 13:16:25 +0800

    first commit

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+
+use std::{io, str::FromStr};
+use chess::{Board, BoardStatus, ChessMove, Square};
+
+use pest::Parser;
+use pest_derive::Parser;
+
+#[derive(Parser)]
+#[grammar = "chess.pest"]
+struct ChessParser;
+
+fn main() {
+    let uci_position = "fen rnbqkbnr/pppppppp/8/8/4P3/8/PPPP1PPP/RNBQKBNR b KQkq - 0 1 moves e2e3 e4e5";
+    let parsed = ChessParser::parse(Rule::game, uci_position).unwrap();
+
+    let mut fen: String = String::new();
+    let mut moves: Vec<String> = Vec::new();
+
+    for pair in parsed {
+        match pair.as_rule() {
+            Rule::game => {
+                for inner_pair in pair.into_inner() {
+                    match inner_pair.as_rule() {
+                        Rule::fen => {
+                            fen = inner_pair.as_str().to_string();
+                        }
+                        Rule::moves => {
+                            moves = inner_pair.into_inner().map(|p| p.as_str().to_string()).collect();
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+
+    let board = Board::from_str(&fen);
+    match board {
+        Ok(board) => {
+            for chess_move in moves {
+                // e2e4 => ChessMove
+                // e2e4Q => ChessMove w/ promotion
+                board.make_move_new();
+            }
+            match board.status() {
+                BoardStatus::Stalemate => println!("result: stalemate"),
+                BoardStatus::Checkmate => println!("result: checkmate"),
+                BoardStatus::Ongoing => println!("result: ongoing"),
+            }
+        },
+        Err(err) => println!("err: {}", err),
+    }
+
+    println!("fen: {}", fen);
+    println!("moves: {:?}", moves);
+
+    /*
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    */
+}
+
